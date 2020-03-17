d4df7105efae0759785b1479675bf030c497ac2b
dogrodnik <d.ogrodnik96@gmail.com>
2020-03-17 10:05:08 +0100

    Updates

---
+++ precache-manifest.6ab7bac5d434426fa4cd8682b822b1cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb842cdfc1109f94cb66cb0f5f30cd86",
+    "url": "/companies-app/index.html"
+  },
+  {
+    "revision": "a950ba2857d095fdfd16",
+    "url": "/companies-app/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "fc4af5c474a0d3f9e960",
+    "url": "/companies-app/static/css/main.6e29f681.chunk.css"
+  },
+  {
+    "revision": "a950ba2857d095fdfd16",
+    "url": "/companies-app/static/js/2.0cd70e6a.chunk.js"
+  },
+  {
+    "revision": "3a9ab140174b67f3a8f4772d5951ac18",
+    "url": "/companies-app/static/js/2.0cd70e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4af5c474a0d3f9e960",
+    "url": "/companies-app/static/js/main.39171ed3.chunk.js"
+  },
+  {
+    "revision": "f6f87e8f896c78a57a2c",
+    "url": "/companies-app/static/js/runtime-main.71f1035f.js"
+  }
+]);
